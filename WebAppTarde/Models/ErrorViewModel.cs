0ca95919c0ba10641116d492ecfa85c4873a59a3
VictoriaKerolyneBM <victoria.kbmedeiros@gmail.com>
2023-03-22 16:14:21 -0300

    Add project files.

---
+++ WebAppTarde/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppTarde.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
