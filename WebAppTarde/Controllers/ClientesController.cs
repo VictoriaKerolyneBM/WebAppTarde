51704db7e15ec82e750cafb436c9aef1dc4ecee2
VictoriaKerolyneBM <victoria.kbmedeiros@gmail.com>
2023-03-29 13:34:16 -0300

    Criação da view lista

--- WebAppTarde/Controllers/ClientesController.cs
+++ WebAppTarde/Controllers/ClientesController.cs
@@ -4,7 +4,7 @@ namespace WebAppTarde.Controllers
 {
     public class ClientesController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Lista()
         {
             return View();
         }
